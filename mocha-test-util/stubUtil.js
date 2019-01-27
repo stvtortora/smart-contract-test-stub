02ad9ba63b8449517d796a2c5d7990122d011fca
Steven <stvtortora@gmail.com>
2019-01-27 17:59:49 -0500

    remove commented out code

--- mocha-test-util/stubUtil.js
+++ mocha-test-util/stubUtil.js
@@ -68,26 +68,6 @@ const getBools = async (stub, methodName) => {
   }
 }
 
-
-// const callHistory = async (stub, methodName) => {
-//   let data = await stub.callHistory(methodName);
-//
-//   return {
-//     firstUint: parseBN(data[0]),
-//     secondUint: parseBN(data[1]),
-//     thirdUint: parseBN(data[2]),
-//     firstString: data[3],
-//     secondString: data[4],
-//     thirdString: data[5],
-//     firstAddress: data[6],
-//     secondAddress: data[7],
-//     thirdAddress: data[8],
-//     called: data[9],
-//     callCount: parseBN(data[10]),
-//     correctCallOrder: data[11]
-//   }
-// }
-
 module.exports = {
   addMethod,
   resetMethod,
